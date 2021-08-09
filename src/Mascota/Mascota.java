f39a781504da0055d6ec010c7f05398fdeeb15c2
luiss <luiss@NanitosaurioPC>
2021-08-08 20:22:43 -0600

    Luis creo energia Signed-off-by:luiss <luiss@NanitosaurioPC>

--- src/Mascota/Mascota.java
+++ src/Mascota/Mascota.java
@@ -5,10 +5,83 @@
  */
 package Mascota;
 
-/**
- *
- * @author luiss
- */
 public class Mascota {
+    private String nombre;
+    private int peso;
+    private float conocimiento;
+    private int cantidadEstudios;
+    private int cantidadAcciones;
+    
+    public Mascota(String nombre) {
+        this.nombre = nombre;
+        this.peso = 500;
+        this.conocimiento = 0;
+        this.cantidadEstudios = 0;
+        this.cantidadAcciones = 0;
+    }
+    
+    public String alimentar(int tipo){
+        if(tipo==1) peso +=30;
+        if(tipo==2) peso +=50;
+        if(tipo==3) peso +=100;
+        
+        this.cantidadEstudios = 0;
+        this.cantidadAcciones++;
+        return "^_^ Ñam Ñam Ñam";
+    }
+    
+    public String ejercitar(int tipo){
+        if(tipo==1) peso -=30;
+        if(tipo==2) peso -=50;
+        if(tipo==3) peso -=100;
+        this.cantidadAcciones++;
+        return "U_U uffff";
+    }
+    
+    public String estudiar(){
+        if(tieneHambre()){
+            return "Tengo hambre";
+        }
+        this.conocimiento+=0.5;
+        this.cantidadEstudios++;
+        this.cantidadAcciones++;
+        return "*_*";
+    }
+    
+    public boolean estaViva(){
+        if (this.peso>1000) return false;
+        if (this.peso<200) return false;
+        if (this.getEdad()== 2) return false;
+        return true;
+    }
+    
+    private int getEdad(){
+        int edad = this.cantidadAcciones/6;
+        return edad;
+    }
+    
+    private boolean tieneHambre(){
+        if(this.cantidadEstudios>=3)return true;
+        return false;
+    }
+    
+    private String getProfesion(){
+        if (this.conocimiento<1) return "Vago";
+        if (this.conocimiento<3) return "Basico";
+        if (this.conocimiento<5) return "Medio";
+        if (this.conocimiento<7) return "Universitario";
+        if (this.conocimiento<9) return "Profesor";
+        if (this.conocimiento<11) return "Cientifico Loco";
+        return "GOD";
+    }
+  
+    
+    public String getEstado(){
+        if(!this.estaViva()){
+            return "D E A D";
+        }
+        return "Nombre:"+this.nombre+"\nPeso:"+this.peso+"\nConocimiento:"+
+                this.conocimiento+"\nProfesion:"+this.getProfesion()+"\nEdad:"+this.getEdad();
+    }
     
 }
---
+++ src/Mascota/NewMain.java
@@ -0,0 +1,90 @@
+package Mascota;
+import java.util.Scanner;
+
+public class NewMain {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        do{
+            opcion=opcionesInicio();
+            if (opcion==1){
+                System.out.println("Nombre mascota");
+                String nombre = sc.next();
+                Mascota mascota = new Mascota(nombre);
+                do{
+                    opcion=opcionesMascota();
+                    
+                    if(opcion==1){
+                        System.out.println("Info");
+                        System.out.println(mascota.getEstado());
+                    }
+                    if(opcion==2){
+                        int alimento = opcionesAlimento();
+                        System.out.println("Alimentando...");
+                        System.out.println(mascota.alimentar(alimento));
+                    }
+                    if(opcion==3){
+                        int ejercicio = opcionesEjercicio();
+                        System.out.println("Ejercitando...");
+                        System.out.println(mascota.ejercitar(ejercicio));
+                    }
+                    if(opcion==4){
+                        System.out.println("Estudiar");
+                        System.out.println(mascota.estudiar());
+                    }
+                }while(opcion!=0 && opcion!=9 && mascota.estaViva());
+                System.out.println(mascota.getEstado());
+            }
+        }while(opcion!=0);
+    }
+    
+    public static int opcionesInicio(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su opcion");
+        System.out.println("1.Crear nueva mascota");
+        System.out.println("0.Salir");
+        return sc.nextInt();
+    }
+    
+    public static int opcionesMascota(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su opcion");
+        System.out.println("1.Estado");
+        System.out.println("2.Alimentar");
+        System.out.println("3.Ejercitar");
+        System.out.println("4.Estudiar");
+        System.out.println("9.Reset");
+        System.out.println("0.Salir");
+        return sc.nextInt();
+    }
+    
+    public static int opcionesAlimento(){
+        Scanner sc = new Scanner(System.in);
+        int op = 0;
+        do{
+            System.out.println("Ingrese su alimento");
+            System.out.println("1.Pan");
+            System.out.println("2.Fideos");
+            System.out.println("3.MC");
+            op = sc.nextInt();
+        }while(op<1 || op>3);
+        
+        return op;
+    }
+    
+    public static int opcionesEjercicio(){
+        Scanner sc = new Scanner(System.in);
+        int op = 0;
+        do{
+            System.out.println("Ingrese su ejercicio");
+            System.out.println("1.Caminar");
+            System.out.println("2.Trotar");
+            System.out.println("3.Correr");
+            op = sc.nextInt();
+        }while(op<1 || op>3);
+        
+        return op;
+    }
+    
+}
